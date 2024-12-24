90cd0476d3685ffd637fcbbbcfd2fe2721b97002
harshil-2507 <harshilandhariya7727@gmail.com>
2024-12-24 09:59:13 +0530

    sign-in also done using appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,7 +23,7 @@ import { Divide, Loader2 } from 'lucide-react'
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
 
 
 const formSchema = z.object({
@@ -33,7 +33,7 @@ const formSchema = z.object({
 
 
 const AuthForm = ({ type }: { type: string }) => {
-    const router = useRouter;
+    const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -75,12 +75,12 @@ const AuthForm = ({ type }: { type: string }) => {
             }
 
             if(type === 'sign-in'){
-                // const response = await signIn({
-                //     email: data.email,
-                //     password: data.password,
-                // })
+                const response = await signIn({
+                    email: data.email!,
+                    password: data.password!,
+                })
 
-                // if(response) router.push('/')
+                if(response) router.push('/')
             }
         } catch (error) {
             console.log(error);
