d8fdf1620d5da8d58ecc30b47416ca8666c09137
harshil-2507 <harshilandhariya7727@gmail.com>
2024-12-24 09:38:48 +0530

    sign up done

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,8 @@ import { ITEMS } from '@/constants'
 import { Divide, Loader2 } from 'lucide-react'
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 
 const formSchema = z.object({
@@ -31,6 +33,7 @@ const formSchema = z.object({
 
 
 const AuthForm = ({ type }: { type: string }) => {
+    const router = useRouter;
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -46,12 +49,44 @@ const AuthForm = ({ type }: { type: string }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    const onSubmit = async(data: z.infer<typeof formSchema>) => {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         setIsLoading(true);
-        console.log(values)
-        setIsLoading(false);
+
+        try {
+            //Sign up with appwrite and create a plaid link token
+
+            if(type === 'sign-up'){
+                const newUser = await signUp({
+                    email: data.email!,
+                    password: data.password!,
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                });
+
+                setUser(newUser);
+            }
+
+            if(type === 'sign-in'){
+                // const response = await signIn({
+                //     email: data.email,
+                //     password: data.password,
+                // })
+
+                // if(response) router.push('/')
+            }
+        } catch (error) {
+            console.log(error);
+        }finally{
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -106,9 +141,15 @@ const AuthForm = ({ type }: { type: string }) => {
                                     </div>
                                     <CustomInput
                                         control={form.control}
-                                        name='address'
+                                        name='address1'
                                         label='Address'
                                         placeholder='Enter your specific Address'
+                                    />
+                                    <CustomInput
+                                        control={form.control}
+                                        name='city'
+                                        label='City'
+                                        placeholder='Enter your city'
                                     />
                                     <div className="flex gap-4">
                                         <CustomInput
